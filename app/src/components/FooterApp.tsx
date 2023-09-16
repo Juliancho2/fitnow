27d73be5965adaeaf6271d521f9afe9999aef9ed
julian moreno <julianmoreno242@gmail.com>
2023-09-16 18:22:22 -0500

    Modiqued UI

--- app/src/components/FooterApp.tsx
+++ app/src/components/FooterApp.tsx
@@ -2,33 +2,38 @@ import React from 'react';
 import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
 
 const FooterApp = () => {
   return (
     <footer className="w-full bg-[#E7E9EE]  py-5">
-      <div className="grid grid-cols-1 w-full max-w-screen-xl mx-auto">
-        <div className="grid grid-cols-2 gap-5 p-5">
-          <div className="flex flex-col gap-1">
-            <div className="flex items-center text-lg text-gray-700">
+      <div className="grid grid-cols-2 w-full max-w-screen-xl mx-auto">
+        <div className="grid grid-cols-2 gap-5 p-5 col-span-2">
+          <div className="flex flex-col gap-2 justify-start">
+            <div className="flex items-center text-sm text-gray-700">
               <FontAwesomeIcon icon={faPhone} className="mr-2" />
               <p>+33758127283</p>
             </div>
-            <div className="flex items-center text-lg text-gray-700">
+            <div className="flex items-center text-sm text-gray-700">
               <FontAwesomeIcon icon={faEnvelope} className="mr-2" />
-              <p>fitnow@gmail.com</p>
+              <p>***@gmail.com</p>
+            </div>
+            <div className="flex items-center gap-5">
+              <Link to={''}>
+                <FontAwesomeIcon icon={faInstagram} className="text-2xl text-gray-700" />
+              </Link>
+              <Link to={''}>
+                <FontAwesomeIcon icon={faFacebook} className="text-2xl text-gray-700" />
+              </Link>
             </div>
           </div>
-          <div className="flex justify-end items-center gap-5 p-5 ">
-            <FontAwesomeIcon icon={faInstagram} className="text-2xl text-gray-700" />
-            <FontAwesomeIcon icon={faFacebook} className="text-2xl text-gray-700" />
+          <div className=" text-end">
+            <p className="text-sm font-bold text-gray-700 my-6">
+              Made by 👽 <a href="https://www.twitter.com/julianchoms" target="_blank" className="text-blue-500 font-normal">@julianchoms</a>
+            </p>
           </div>
         </div>
-        <div className="col-span-2 text-center">
-          <p className="text-lg font-bold text-gray-700 my-6">
-            Made by 👽 <a href="https://www.twitter.com/julianchoms" target="_blank" className="text-blue-500">@julianchoms</a>
-          </p>
-          <p className="text-sm text-gray-700">© 2023 All rights reserved.</p>
-        </div>
+        <p className="text-sm text-center text-gray-700 col-span-2">© 2023 All rights reserved.</p>
       </div>
     </footer>
   );

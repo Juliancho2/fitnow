bfb8801b7e087637a766041ea56e49bce44d1326
julian moreno <julianmoreno242@gmail.com>
2023-09-16 18:07:36 -0500

    Migrated from styled component to tailwind

--- app/src/components/MessageErrorValidate.tsx
+++ app/src/components/MessageErrorValidate.tsx
@@ -1,25 +1,16 @@
 import React from 'react'
-import styled from 'styled-components'
 
 interface Props {
     message: Array<string> | []
 }
 
-const Container = styled.div`
-    position: absolute;
-    bottom: -50px;
-    text-align: center;
-    &>p{
-        color: #f44343;
-    }
-`
 const MessageErrorValidate = ({ message = [] }: Props) => {
     return (
-        <Container className='message-error'>
+        <div className='  text-center'>
             {
-                message.map((message, index) => (<p key={index}>* {message}</p>))
+                message.map((message, index) => (<p className='text-red-500' key={index}>* {message}</p>))
             }
-        </Container>
+        </div>
     )
 }
 

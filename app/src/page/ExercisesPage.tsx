7a83dc8921ce3bff5f4a9752068c310ab4a05827
julian moreno <julianmoreno242@gmail.com>
2023-09-16 18:09:59 -0500

    Dispatch is called to fetch data from bodyTarget

--- app/src/page/ExercisesPage.tsx
+++ app/src/page/ExercisesPage.tsx
@@ -1,17 +1,29 @@
-
-
+import { useDispatch, useSelector } from 'react-redux'
 import HeaderApp from '../components/HeaderApp'
 import ListOfBodyParts from '../components/ListOfBodyParts'
-import { bodyParts } from '../static'
+import { AppDispatch } from '../redux/store/store'
+import { fetchBodyTarget } from '../redux/thunk'
+import { useEffect } from 'react'
+import { RootState } from '../interface'
+import SpinnerComponent from '../components/SpinnerComponent'
 
 const ExercisesPage = () => {
+    const dispatch = useDispatch<AppDispatch>()
+    const {bodyItems}=useSelector((state:RootState)=>state.bodyTarget)
+
+    useEffect(() => {
+        dispatch(fetchBodyTarget())
+    }, [])
+
     return (
         <div className='min-h-[100vh] relative'>
             <HeaderApp />
             <div className='absolute opacity-10'>
                 <img src="https://district0x.io/images/hero-blobs.png" alt="" />
             </div>
-            <ListOfBodyParts data={bodyParts} />
+            
+            <ListOfBodyParts data={bodyItems} />
+            
         </div>
     )
 }

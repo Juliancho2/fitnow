a0f9cc327ab227e6d0e4db7d0cf23ad1f231cde2
julian moreno <julianmoreno242@gmail.com>
2023-09-19 11:36:10 -0500

    Parameter validation removed

--- app/src/page/ExerciseFoundsPage.tsx
+++ app/src/page/ExerciseFoundsPage.tsx
@@ -7,18 +7,18 @@ import { RootState } from '../interface'
 import CardExercise from '../components/CardExercise'
 import ModalAddExercise from '../components/ModalAddExercise'
 import { AppDispatch } from '../redux/store/store'
+import SpinnerComponent from '../components/SpinnerComponent'
 
 const ExerciseFoundsPage = () => {
   const params = useParams()
   const dispath = useDispatch<AppDispatch>()
-  const { dataSearch, exerciseToAdd } = useSelector((state: RootState) => state.dataApi)
+  const { dataSearch, exerciseToAdd, isLoading } = useSelector((state: RootState) => state.dataApi)
 
 
 
   useEffect(() => {
-   if(params.target){
-    dispath(fetchExerciseDbApi({ name: params.target }))
-   }
+    dispath(fetchExerciseDbApi({ name: params.name }))
+
   }, [])
 
   return (
@@ -31,10 +31,13 @@ const ExerciseFoundsPage = () => {
         <div className='absolute opacity-5'>
           <img src="https://district0x.io/images/hero-blobs.png" alt="" />
         </div>
-        <h2 className='py-40 text-4xl max-w-screen-xl mx-auto font-bold'>Results: {params.id}</h2>
+        <h2 className='py-40 text-4xl max-w-screen-xl mx-auto font-bold'>Results: {params.name}</h2>
         <div className='max-w-screen-xl pb-40 mx-auto grid sm:grid-cols-2 justify-center md:grid-cols-4 gap-4 relative z-10'>
+
+          {isLoading && <SpinnerComponent styles='border-4 border-sky-600 animate-spin col-span-full w-6 h-8 rounded-full mx-auto' />}
+
           {
-            dataSearch.length > 0 && dataSearch.map((exercise) => (
+            (dataSearch.length > 0 && !isLoading) && dataSearch.map((exercise) => (
               <CardExercise exerciseData={exercise} />
             ))
           }
